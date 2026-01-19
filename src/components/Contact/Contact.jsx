5e0ce37728004da3842b20f0d9fbb0bb66e0b4cc
Alberto <albertolaniman@gmail.com>
2026-01-18 21:53:22 -0600

    feat: installed eslint and update my cv

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,34 +10,19 @@ export const Contact = () => {
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
-          <img
-            loading="lazy"
-            src={imgSrc(`contact/emailIcon`)}
-            alt="Email icon"
-          />
+          <img loading="lazy" src={imgSrc(`contact/emailIcon`)} alt="Email icon" />
           <a href="mailto:albertolaniman@gmail.com">albertolaniman@gmail.com</a>
         </li>
 
         <li className={styles.link}>
-          <img
-            loading="lazy"
-            src={imgSrc(`contact/linkedinIcon`)}
-            alt="Linkedin icon"
-          />
-          <a
-            target="_blank"
-            href="https://www.linkedin.com/in/alberto-zelaya-b2a3ba210/"
-          >
+          <img loading="lazy" src={imgSrc(`contact/linkedinIcon`)} alt="Linkedin icon" />
+          <a target="_blank" href="https://www.linkedin.com/in/alberto-zelaya-b2a3ba210/">
             linkedin.com/albertozelaya
           </a>
         </li>
 
         <li className={styles.link}>
-          <img
-            loading="lazy"
-            src={imgSrc(`contact/githubIcon`)}
-            alt="Github icon"
-          />
+          <img loading="lazy" src={imgSrc(`contact/githubIcon`)} alt="Github icon" />
           <a target="_blank" href="https://github.com/albertozelaya5">
             github.com/albertozelaya5
           </a>
